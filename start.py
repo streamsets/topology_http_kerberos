1857b69f4f559a37dbed7ab8d19d989cb76b9f05
Shashi Mazumdar <shashi@streamsets.com>
2020-07-07 06:19:20 -0700

    Update topology_http_kerberos to add an SSL virtual host. (#3)

--- start.py
+++ start.py
@@ -35,20 +35,25 @@ KDC_KRB5_CONF_FILENAME = '/etc/krb5.conf'
 def main(args):
     kerberos_volume_dir = os.path.expanduser(args.kerberos_config_directory or args.clusterdock_config_directory)
 
-    #kdc_image = '{}/{}/topology_http_kerberos:kdc'.format(args.registry,
-    #                                                      args.namespace or DEFAULT_NAMESPACE)
+    # kerberos node.
     kdc_image = '{}/clusterdock/topology_nodebase_kerberos:centos6.8'.format(args.registry)
     kdc_hostname = args.kdc_node[0]
     kdc_node = Node(hostname=kdc_hostname, group='kdc', image=kdc_image,
                     volumes=[{kerberos_volume_dir: KERBEROS_VOLUME_DIR}])
 
+    # webserver node. this is the reverse proxy that exposes the URLs
     webserver_image = '{}/{}/topology_http_kerberos:webserver'.format(args.registry,
                                                                       args.namespace or DEFAULT_NAMESPACE)
     webserver_hostname = args.webserver_node[0]
     webserver_node = Node(hostname=webserver_hostname, group='webserver', image=webserver_image,
-                          volumes=[{kerberos_volume_dir: KERBEROS_VOLUME_DIR}], ports={80: 80})
+                          volumes=[{kerberos_volume_dir: KERBEROS_VOLUME_DIR}], ports={80: 80, 443: 443})
 
-    cluster = Cluster(kdc_node, webserver_node)
+    # service node. the actual service (in our case, pretenders, which allows us to create mock http urls)
+    service_hostname = args.service_node[0]
+    service_node = Node(hostname=service_hostname, group='service', image='pretenders/pretenders:1.4',
+                        ports={8000: 8000})
+
+    cluster = Cluster(kdc_node, webserver_node, service_node)
     cluster.start(args.network)
 
     logger.info('Updating KDC configurations ...')
@@ -110,6 +115,13 @@ def main(args):
 
     logger.info('Validating kerberos service health ...')
     _validate_service_health(node=kdc_node, services=['krb5kdc', 'kadmin'], quiet=not args.verbose)
+
+    # copy self signed certificate and private key from image to clusterdock config location. Any consumer
+    # can then import the certificate as a trusted certificate.
+    webserver_node.execute('cp /etc/ssl/certs/selfsigned.crt {ssl_cert_dir}/selfsigned.crt '
+                           '&& cp /etc/ssl/private/private.key {ssl_cert_dir}/private.key'.format(
+                               ssl_cert_dir=KERBEROS_VOLUME_DIR),
+                           quiet=not args.verbose)
 
     # copy the krb5.conf file from the shared location to /etc on the webserver node and start the webserver.
     webserver_node.execute('cp -p {}/krb5.conf {}'.format(KERBEROS_VOLUME_DIR, KDC_KRB5_CONF_FILENAME),
