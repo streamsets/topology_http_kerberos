563ff15ff96718f7be790cc348fd381a7f44f47c
Kenai Rhodes <river@streamsets.com>
2024-10-14 13:21:34 -0500

    TEST-4469. Migrate HTTP Kerberos topology to use Podman
    Change-Id: I46e05272f25d2f0a733be86b6bb56fe767f885ba
    Reviewed-on: https://review.streamsets.net/c/topology_http_kerberos/+/78395
    Reviewed-by: Dean Proctor <dean@streamsets.com>

--- images/build_images.sh
+++ images/build_images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #pushd kdc
-#docker build . -t clusterdock/topology_http_kerberos:kdc
+#podman build . -t clusterdock/topology_http_kerberos:kdc
 #popd
 
 pushd webserver
-docker build . -t clusterdock/topology_http_kerberos:webserver
+podman build . -t clusterdock/topology_http_kerberos:webserver
 popd
